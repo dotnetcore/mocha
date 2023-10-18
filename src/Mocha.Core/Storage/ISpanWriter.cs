30d97a7374ad6a8e686d655f42e1ff382f6c63a4
Liu Haoyang <liuhaoyang1221@hotmail.com>
2023-10-18 13:54:14 +0800

    Add license header (#24)

--- src/Mocha.Core/Storage/ISpanWriter.cs
+++ src/Mocha.Core/Storage/ISpanWriter.cs
@@ -1,4 +1,7 @@
-﻿namespace Mocha.Core.Storage;
+﻿// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
+namespace Mocha.Core.Storage;
 
 public interface ISpanWriter
 {
