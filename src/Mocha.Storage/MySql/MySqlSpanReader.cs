be6413211dc84dbf0cf0cc5744a477aa544295a6
tsaiggo <ttsaiggo@gmail.com>
2023-10-18 17:36:49 +0800

    Format/check style (#27)

--- src/Mocha.Storage/MySql/MySqlSpanReader.cs
+++ src/Mocha.Storage/MySql/MySqlSpanReader.cs
@@ -5,9 +5,8 @@ using Mocha.Core.Storage;
 
 namespace Mocha.Storage.Mysql;
 
-public class MySqlSpanReader:ISpanReader
+public class MySqlSpanReader : ISpanReader
 {
-
     public Task FindTraceList(string serviceName)
     {
         throw new NotImplementedException();
