292cb87dd2ae4aa2c5fb27ede9a34466ad53ac6d
kawhi <1585955375@qq.com>
2023-10-22 09:29:41 +0800

    Fix whitespace formatting. Insert '\s'.

--- src/Mocha.Storage/EntityFrameworkStorage/Trace/SpanAttribute.cs
+++ src/Mocha.Storage/EntityFrameworkStorage/Trace/SpanAttribute.cs
@@ -5,15 +5,15 @@ namespace Mocha.Storage.EntityFrameworkStorage.Trace;
 
 public class SpanAttribute
 {
-    public string AttributeKey { get; private set; }= default!;
+    public string AttributeKey { get; private set; } = default!;
 
-    public string AttributeValue { get; private set; }= default!;
+    public string AttributeValue { get; private set; } = default!;
 
     public long TimeBucket { get; private set; }
 
-    public string TraceId { get; private set; }= default!;
+    public string TraceId { get; private set; } = default!;
 
-    public string SpanId { get; private set; }= default!;
+    public string SpanId { get; private set; } = default!;
 
     public Span Span { get; set; } = default!;
 }
--- src/Mocha.Storage/EntityFrameworkStorage/Trace/SpanEvent.cs
+++ src/Mocha.Storage/EntityFrameworkStorage/Trace/SpanEvent.cs
@@ -3,13 +3,14 @@
 
 
 namespace Mocha.Storage.EntityFrameworkStorage.Trace;
+
 public class SpanEvent
 {
-    public string TraceId { get; private set; }= default!;
+    public string TraceId { get; private set; } = default!;
 
     public long TimeBucket { get; private set; }
 
-    public string EventName { get; private set; }= default!;
+    public string EventName { get; private set; } = default!;
 
     public Span Span { get; set; } = default!;
 }
