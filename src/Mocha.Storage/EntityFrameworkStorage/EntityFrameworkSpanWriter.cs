223cdb5c6ca0fa02136735bed8f9df4ac6b40c94
kawhi <1585955375@qq.com>
2023-10-23 20:29:21 +0800

    Fixed: clean file_header_template code license

--- src/Mocha.Storage/EntityFrameworkStorage/EntityFrameworkSpanWriter.cs
+++ src/Mocha.Storage/EntityFrameworkStorage/EntityFrameworkSpanWriter.cs
@@ -1,5 +1,5 @@
-// Licensed to the.NET Core Community under one or more agreements.
-// The.NET Core Community licenses this file to you under the MIT license.
+// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
 
 using Mocha.Core.Storage;
 
--- src/Mocha.Storage/MochaContext.cs
+++ src/Mocha.Storage/MochaContext.cs
@@ -1,5 +1,5 @@
-// Licensed to the.NET Core Community under one or more agreements.
-// The.NET Core Community licenses this file to you under the MIT license.
+// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
 
 using Microsoft.EntityFrameworkCore;
 using Mocha.Storage.EntityFrameworkStorage.Trace;
