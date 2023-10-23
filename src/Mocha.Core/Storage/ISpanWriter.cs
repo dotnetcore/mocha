a17af3ff5077bb186d344775c7f04238d1d6389a
kawhi <46982191+KawhiWei@users.noreply.github.com>
2023-10-23 08:50:43 -0500

    Span interface definition, Span model definition added (#25)
    1. Changes within the Mocha.Storage MysqlSpanWrite,
    EntityFrameworkSpanWrite MySQLSpanReader EntityFrameworkSpanReader
    2. Changes within the Mocha.Core.Storage ISpanWriter, ISpanWrite
    3. Add MochaContext definition
    4. https://github.com/dotnetcore/mocha/issues/7

--- src/Mocha.Core/Storage/ISpanWriter.cs
+++ src/Mocha.Core/Storage/ISpanWriter.cs
@@ -1,11 +1,11 @@
 // Licensed to the .NET Core Community under one or more agreements.
 // The .NET Core Community licenses this file to you under the MIT license.
 
+using OpenTelemetry.Proto.Trace.V1;
+
 namespace Mocha.Core.Storage;
 
 public interface ISpanWriter
 {
-    Task<bool> WriterAsync();
-
-    bool Writer();
+    Task WriteAsync(IEnumerable<Span> spans);
 }
---
+++ src/Mocha.Storage/EntityFrameworkStorage/Trace/Span.cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
+using Mocha.Core.Enums;
+
+namespace Mocha.Storage.EntityFrameworkStorage.Trace;
+
+public class Span
+{
+    public string TraceId { get; set; } = string.Empty;
+
+    public string SpanId { get; set; } = string.Empty;
+
+    public string SpanName { get; set; } = string.Empty;
+
+    public string ParentSpanId { get; set; } = string.Empty;
+
+    public string ServiceName { get; set; } = string.Empty;
+
+    public long StartTime { get; set; }
+
+    public long EndTime { get; set; }
+
+    public double Duration { get; set; }
+
+    public int StatusCode { get; set; }
+
+    public string? StatusMessage { get; set; } = string.Empty;
+
+    public SpanKind SpanKind { get; set; }
+
+    public uint TraceFlags { get; set; }
+
+    public string? TraceState { get; set; }
+
+    public IEnumerable<SpanLink> SpanLinks { get; set; } = Enumerable.Empty<SpanLink>();
+
+    public IEnumerable<SpanAttribute> SpanAttributes { get; set; } = Enumerable.Empty<SpanAttribute>();
+
+    public IEnumerable<SpanEvent> SpanEvents { get; set; } = Enumerable.Empty<SpanEvent>();
+}
