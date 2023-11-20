568ad82ffbea1cdf50bb5d39936351edd5cd3bb6
tsaiggo <ttsaiggo@gmail.com>
2023-11-20 22:43:12 +0800

    :arrow_up: Upgrade dotnet version

--- src/Mocha.Core/Storage/Query/TraceReadQuery.cs
+++ src/Mocha.Core/Storage/Query/TraceReadQuery.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Core Community under one or more agreements.
 // The .NET Core Community licenses this file to you under the MIT license.
 
-using System.Collections;
-
 namespace Mocha.Core.Storage.Query;
 
 public class TraceReadQuery
