c55646e76f295068e18deb992fbf0643810a9791
kawhi <1585955375@qq.com>
2023-11-24 20:45:24 +0800

    删除无用警告

--- src/Mocha.Storage/StorageServiceCollectionExtensions.cs
+++ src/Mocha.Storage/StorageServiceCollectionExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Core Community under one or more agreements.
 // The .NET Core Community licenses this file to you under the MIT license.
 
-using Microsoft.Extensions.DependencyInjection;
 using Mocha.Storage;
 
 namespace Microsoft.Extensions.DependencyInjection;
