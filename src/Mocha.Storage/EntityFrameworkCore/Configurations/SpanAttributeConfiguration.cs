e95e7e98d08cc84237403e7dceb1d8fc63b7750c
kawhi <1585955375@qq.com>
2023-11-26 19:33:47 +0800

    featrue 解决bom问题

--- src/Mocha.Storage/EntityFrameworkCore/Configurations/SpanAttributeConfiguration.cs
+++ src/Mocha.Storage/EntityFrameworkCore/Configurations/SpanAttributeConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Mocha.Storage.EntityFrameworkCore.Trace;
 
--- src/Mocha.Storage/StorageServiceCollectionExtensions.cs
+++ src/Mocha.Storage/StorageServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Mocha.Storage;
+// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
+using Mocha.Storage;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
