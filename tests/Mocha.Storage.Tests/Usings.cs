5cd2756a584e468cf8159dde3f3e32e866135316
kawhi <1585955375@qq.com>
2023-11-28 13:43:44 +0800

    feature 1、添加Storage的单元测试

---
+++ tests/Mocha.Storage.Tests/Usings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Mocha.Storage.EntityFrameworkCore;
+global using Mocha.Storage.EntityFrameworkCore.Trace;
+
