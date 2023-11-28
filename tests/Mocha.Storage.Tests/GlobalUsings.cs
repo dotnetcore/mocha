512e9db4e644e23060904f0125a0f977446ccf11
kawhi <1585955375@qq.com>
2023-11-28 13:48:13 +0800

    fixed 1、修复报错

--- tests/Mocha.Storage.Tests/GlobalUsings.cs
+++ tests/Mocha.Storage.Tests/GlobalUsings.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
 global using Xunit;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
