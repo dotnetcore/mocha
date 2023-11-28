9557f70fb257c6dcf6f312e6b2ffa2f4f6c59198
kawhi <1585955375@qq.com>
2023-11-28 14:00:04 +0800

    feature

--- tests/Mocha.Storage.Tests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-// Licensed to the .NET Core Community under one or more agreements.
-// The .NET Core Community licenses this file to you under the MIT license.
-
-global using Xunit;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Diagnostics;
-global using Mocha.Storage.EntityFrameworkCore;
-global using Mocha.Storage.EntityFrameworkCore.Trace;
-
