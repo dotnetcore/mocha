317bb54b0769ab564447e8b0be217bb1e1ef9c86
kawhi <1585955375@qq.com>
2023-11-28 15:40:37 +0800

    feature 1、添加部分单元测试代码

---
+++ tests/Mocha.Storage.Tests/EntityFrameworkCore/EntityFrameworkCoreServiceCollectionExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using Mocha.Core.Buffer;
+using Mocha.Storage.EntityFrameworkCore;
+
+namespace Mocha.Storage.Tests.EntityFrameworkCore;
+
+public class EntityFrameworkCoreServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddMemoryBuffer()
+    {
+        var services = new ServiceCollection();
+        services.AddStorage(x =>
+        {
+            x.UseEntityFrameworkCore();
+            x.Services.AddDbContext<MochaContext>();
+        });
+
+    }
+}
---
+++ tests/Mocha.Storage.Tests/EntityFrameworkCore/InMemoryMochaContextTest.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Mocha.Storage.EntityFrameworkCore;
+using Mocha.Storage.EntityFrameworkCore.Trace;
+
+namespace Mocha.Storage.Tests.EntityFrameworkCore;
+
+public class InMemoryMochaContextTest
+{
+    private readonly DbContextOptions<MochaContext> _contextOptions;
+    public InMemoryMochaContextTest()
+    {
+        _contextOptions = new DbContextOptionsBuilder<MochaContext>()
+            .UseInMemoryDatabase("InMemoryMochaContextTest")
+            .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+    }
+
+    [Fact]
+    public async Task CreateDatabase()
+    {
+        await using var context = new MochaContext(_contextOptions);
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+        context.AddRange();
+        await context.SaveChangesAsync();
+    }
+
+
+    [Fact]
+    public async Task AddSpanAsync()
+    {
+        await using var context = new MochaContext(_contextOptions);
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+        context.Spans.Add(SpanBase.CreateSpan());
+        await context.SaveChangesAsync();
+    }
+}
