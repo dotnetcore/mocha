cd49b61ee4c07c72f56133eff2c545ac207d84c3
kawhi <1585955375@qq.com>
2023-11-28 15:01:10 +0800

    add 添加storage单元测试类库

---
+++ tests/Mocha.Storage.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+// Licensed to the .NET Core Community under one or more agreements.
+// The .NET Core Community licenses this file to you under the MIT license.
+
+namespace Mocha.Storage.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
