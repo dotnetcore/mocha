fc86f13f7a56788908c9c8f21638a1a835aeebbd
kawhi <1585955375@qq.com>
2023-12-09 15:45:20 +0800

    调整顺序

--- src/Mocha.Storage/EntityFrameworkCore/EFConversionExtensions.cs
+++ src/Mocha.Storage/EntityFrameworkCore/EFConversionExtensions.cs
@@ -1,15 +1,11 @@
 // Licensed to the .NET Core Community under one or more agreements.
 // The .NET Core Community licenses this file to you under the MIT license.
-
+using System.Text;
+using Mocha.Storage.EntityFrameworkCore.Trace;
 using OTelSpan = OpenTelemetry.Proto.Trace.V1.Span;
 using OTelLink = OpenTelemetry.Proto.Trace.V1.Span.Types.Link;
 using OTelEvent = OpenTelemetry.Proto.Trace.V1.Span.Types.Event;
 using OTelKeyValue = OpenTelemetry.Proto.Common.V1.KeyValue;
-using OTelSpanKind = OpenTelemetry.Proto.Trace.V1.Span.Types.SpanKind;
-using System.Text;
-using Mocha.Core.Enums;
-using Mocha.Storage.EntityFrameworkCore.Trace;
-
 namespace Mocha.Storage.EntityFrameworkCore;
 public static class EFConversionExtensions
 {
