a12b47d15aeda06df22874432c4596216eb4ffea
Event Horizon <772552754@qq.com>
2025-12-28 23:15:25 +0800

    formart

--- tests/Mocha.Query.Tests/Prometheus/Engine/OperatorTests.cs
+++ tests/Mocha.Query.Tests/Prometheus/Engine/OperatorTests.cs
@@ -51,7 +51,8 @@ public class OperatorTests
         var mockOptions = new Mock<IOptions<PromQLEngineOptions>>();
         mockOptions.SetupGet(x => x.Value).Returns(new PromQLEngineOptions
         {
-            DefaultEvaluationInterval = TimeSpan.FromSeconds(15), MaxSamplesPerQuery = 50000000
+            DefaultEvaluationInterval = TimeSpan.FromSeconds(15),
+            MaxSamplesPerQuery = 50000000
         });
 
         var engine = new PromQLEngine(new MochaPromQLParserParser(), new InMemoryPrometheusMetricsReader(series),
